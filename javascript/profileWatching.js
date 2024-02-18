942d2a51b914fcaaffbeff9ba57dd75282b8a8b0
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-02-18 20:00:57 +0000

    small design fixes and profile pages as well as making modular code

---
+++ javascript/profileWatching.js
@@ -0,0 +1,19 @@
+function loadPage(pageName, element) {
+    fetch(pageName)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(element).innerHTML = html;
+        })
+        .catch(error => console.error('Error fetching header:', error));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    loadPage('../html/top-header-login.html', 'header');
+    loadPage('../html/bottom-footer.html', 'footer');
+    loadPage('../html/profile/profile-table-counter.html', 'table');
+    loadPage('../html/profile/profile-nav.html', 'profile-nav');
+    loadPage('../html/profile/profile-info.html', 'profile-info');
+
+
+});
