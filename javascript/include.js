8ca194ee53df930517627be04eae1e61bf09322c
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-02-29 15:01:27 +0000

    Working Async Tasks But Not Properly Done

--- javascript/include.js
+++ javascript/include.js
@@ -1,15 +1,15 @@
-function loadById(pageName, element) {
-    fetch(pageName)
-        .then(response => response.text())
+async function loadById(pageName, element) {
+    await fetch(pageName)
+        .then(async response => await response.text())
         .then(html => {
             document.getElementById(element).innerHTML = html;
         })
         .catch(error => console.error('Error fetching header:', error));
 }
 
-function loadByClass(pageName, className) {
-    fetch(pageName)
-        .then(response => response.text())
+async function loadByClass(pageName, className) {
+    await fetch(pageName)
+        .then(async response => await response.text())
         .then(html => {
             let elements = document.getElementsByClassName(className);
             for (let i = 0, length = elements.length; i < length; i++) {
