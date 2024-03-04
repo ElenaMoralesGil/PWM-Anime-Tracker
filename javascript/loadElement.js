2f95f0264a1f90bad25dd82acaa4044a6d898413
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-03-04 16:37:29 +0000

    added unversioned files to be versioned

--- javascript/loadElement.js
+++ javascript/loadElement.js
@@ -4,14 +4,20 @@ function loadElement(url, containerId, elementSelector) {
         .then(html => {
             const tempContainer = document.createElement('div');
             tempContainer.innerHTML = html.trim();
-            const element = tempContainer.querySelector(elementSelector);
-            if (element) {
+            const elements = tempContainer.querySelectorAll(elementSelector);
+            if (elements.length > 0) {
                 const fragment = document.createDocumentFragment(); // Create a document fragment
-                fragment.appendChild(element.cloneNode(true)); // Append a clone of the element to the fragment
+                let count = 0; // Initialize a count variable
+                elements.forEach(element => {
+                    if (count < 5) { // Check if the count is less than 5
+                        fragment.appendChild(element.cloneNode(true)); // Append a clone of each element to the fragment
+                        count++; // Increment the count
+                    }
+                });
                 const container = document.getElementById(containerId);
                 container.appendChild(fragment); // Append the fragment to the container
             } else {
-                console.error('Element not found in the fetched content.');
+                console.error('Elements not found in the fetched content.');
             }
         })
         .catch(error => console.error('Error fetching content:', error));
