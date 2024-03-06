89336c660708cd3b936d494d0696f5feae2301f8
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-03-06 09:59:40 +0000

    Characters-Page Becoming Full Dynamically Loaded!

---
+++ javascript/templates/anime-character.js
@@ -0,0 +1,22 @@
+function loadCharacterName(element) {
+    element.querySelector(".anime-character-name-image p").innerHTML="Text";
+}
+
+function loadCharacterImage(element) {
+    element.querySelector(".anime-character-name-image img").src="../../resources/images/frieren.jpg";
+}
+
+function loadCharacterDescription(element) {
+    element.querySelector(".character-description").innerHTML=
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n" +
+        "sed do eiusmod tempor incididunt ut labore et dolore magna\n" +
+        "aliqua. Ut enim ad minim veniam, quis nostrud exercitation\n" +
+        "ullamco laboris nisi ut aliquip ex ea commodo consequat\n" +
+        "Duis aute irure dolor in reprehenderit.";
+}
+
+function loadAnimeCharacter(element) {
+    loadCharacterName(element);
+    loadCharacterImage(element);
+    loadCharacterDescription(element);
+}
