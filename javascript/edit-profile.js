e6afb8f90ed6d16d93a99406f06cdb197d31000a
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-03-06 20:53:00 +0000

    improved edit-profle design and  " choose profile picture" functionality

---
+++ javascript/edit-profile.js
@@ -0,0 +1,16 @@
+function updateProfilePicture() {
+    const fileInput = document.getElementById('profileImage');
+    const chosenImage = document.getElementById('chosenImage');
+
+    const file = fileInput.files[0];
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            chosenImage.src = e.target.result;
+            chosenImage.style.display = 'block'; // Show the image
+        }
+
+        reader.readAsDataURL(file);
+    }
+}
