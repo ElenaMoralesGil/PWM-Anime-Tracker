261199863413d697a3d0814d419d51ca26e6978b
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-03-08 03:13:14 +0000

    Ranking Page Done!

---
+++ javascript/ranking-page/ranking-page.js
@@ -0,0 +1,31 @@
+function loadRankingPositions() {
+    const positionRoute = "../../html/templates/ranking-position.html";
+    let positionHTML = undefined;
+
+    let positionFetch = new Promise((resolve) => {
+        fetch(positionRoute).
+        then(response => response.text()).
+        then(html => {
+            positionHTML = html;
+            resolve();
+        })
+    })
+
+    positionFetch.then(()=>{
+        let documentFragment = new DocumentFragment();
+        for (let i = 0; i < 20; i++){
+            let positionContainer = document.createElement("article");
+            positionContainer.className="anime-position";
+            positionContainer.innerHTML=positionHTML;
+            loadRankingPosition(positionContainer);
+            documentFragment.appendChild(positionContainer);
+        }
+        document.querySelector(".positions").appendChild(documentFragment);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadById('../templates/top-header.html', 'header').then(/*doNothing*/)
+    loadById('../templates/bottom-footer.html', 'footer').then(/*doNothing*/);
+    loadRankingPositions();
+});
