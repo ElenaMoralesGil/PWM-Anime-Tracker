a8b2e9216cde81646801464feee33d7d45b31cb0
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-03-20 15:01:35 +0000

    Header Mobile and Suggested Modal Views

--- javascript/index/index.js
+++ javascript/index/index.js
@@ -22,10 +22,14 @@ function addHeroImageEvent() {
     })
 }
 
+function loadMobileMenu() {
+    return loadById('../../html/templates/mobile-header-menu.html', 'mobile-menu');
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    loadTopHeader().then(addHeaderEvent);
-    loadById('../templates/bottom-footer.html', 'footer').then(/*doNothing*/)
-    loadById('../templates/name-image.html', 'index-main-content-hero-image').
+    loadTopHeader().then(addHeaderEvent).then(loadMobileMenu).then(mobileMenuEvent);
+    loadById('../../html/templates/bottom-footer.html', 'footer').then(/*doNothing*/)
+    loadById('../../html/templates/name-image.html', 'index-main-content-hero-image').
     then(loadHeroImage).
     then(addHeroImageEvent);
     initSearchBar();
