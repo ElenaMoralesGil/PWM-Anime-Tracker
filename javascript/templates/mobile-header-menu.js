a8cd3427f112c41cd5c9610e21b59b885319b002
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-03-24 12:55:50 +0000

    now header and mobile header in profile pages work

--- javascript/templates/mobile-header-menu.js
+++ javascript/templates/mobile-header-menu.js
@@ -68,7 +68,7 @@ function loadMenuRedirectionsLogin() {
 
     document.querySelector("#logout").addEventListener("click", () => {
         if(document.querySelector("body").id !== "index-body"){
-            location.assign("/html/index/index.html");
+            location.assign("../../html/index-page/index.html");
         }
     });
 
