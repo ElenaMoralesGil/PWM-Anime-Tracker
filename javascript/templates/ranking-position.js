d9d1f0748f70b2c5715a36c8359f7204459a8b9a
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-03-25 01:20:37 +0000

    ranking-page done

--- javascript/templates/ranking-position.js
+++ javascript/templates/ranking-position.js
@@ -1,7 +1,7 @@
 function loadRankingPosition(positionContainer){
     let rankingData = positionContainer.querySelectorAll("p");
     for (let data of rankingData){
-        data.innerHTML="Text";
+        data.innerHTML="Ano Hi Mita Hana No Wa Bokutachi Wa Mada Shiranai";
     }
     let image = positionContainer.querySelector("img");
     image.alt="ranking anime cover";
