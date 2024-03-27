2231348a97c883fddf62ecc38fdd6a4c593c5c23
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-03-27 18:12:51 +0000

    character description done

--- javascript/templates/anime-character.js
+++ javascript/templates/anime-character.js
@@ -1,5 +1,7 @@
+import CharacterModel from "../../models/charactermodel.js";
+const model = new CharacterModel();
+
 function loadCharacterName(element, characterData) {
-    console.log(characterData)
     element.querySelector(".anime-character-name-image p").innerHTML= characterData ? characterData.character.name : "Text";
 }
 
@@ -7,17 +9,25 @@ function loadCharacterImage(element, characterData) {
     element.querySelector(".anime-character-name-image img").src= characterData ? characterData.character.images.jpg.image_url : "../../../resources/images/frieren.jpg";
 }
 
-function loadCharacterDescription(element) {
-    element.querySelector(".character-description").innerHTML=
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n" +
-        "sed do eiusmod tempor incididunt ut labore et dolore magna\n" +
-        "aliqua. Ut enim ad minim veniam, quis nostrud exercitation\n" +
-        "ullamco laboris nisi ut aliquip ex ea commodo consequat\n" +
-        "Duis aute irure dolor in reprehenderit.";
+function loadCharacterDescription(element, characterData) {
+    element.addEventListener("click", () => {
+        let description = element.querySelector(".character-description");
+        let name = element.querySelector(".anime-character-name-image p");
+        if(description.classList.contains("hidden")){
+            name.classList.add("hidden");
+            model.characterInfo(characterData.character.mal_id).then(character => {
+                description.innerHTML = character.description;
+                description.classList.remove("hidden");
+            })
+        } else {
+            name.classList.remove("hidden");
+            description.classList.add("hidden");
+        }
+    })
 }
 
 export default function loadAnimeCharacter(element, characterData) {
     loadCharacterName(element, characterData);
     loadCharacterImage(element, characterData);
-    loadCharacterDescription(element);
+    loadCharacterDescription(element, characterData);
 }
